1251ee83c27eae0c37f5d2e9e0ef959c01df1f6a
MichaelLong <michaellong971105@gmail.com>
2025-05-06 20:59:45 +1200

    用于检查npy文件是否符合要求

--- training_record/checknpy.py
+++ training_record/checknpy.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-X = np.load("X_train.npy")
-y = np.load("y_train.npy")
-print("X.shape =", X.shape)
-print("y.shape =", y.shape)
+X = np.load("../predicting_record/knn_model/y_pred_r_1_to_9_knn_merged_20250505_220256.npy")
+# y = np.load("y_train.npy")
+print(X)
+# print("y.shape =", y.shape)
