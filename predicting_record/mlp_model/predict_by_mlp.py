ad7c8dd062edcbf2fc80b84edd22299f29ac35b6
MichaelLong <michaellong971105@gmail.com>
2025-05-06 21:01:37 +1200

    修改代码，尝试使用模型1预测的结果用于模型2，以此预测r_{1}_pos_{1} ~ r_{9}_pos_{300}的结果

--- predicting_record/mlp_model/predict_by_mlp.py
+++ predicting_record/mlp_model/predict_by_mlp.py
@@ -23,16 +23,26 @@ train_dataset = "train_raw.csv"  # use the dataset train.csv which is built by '
 train_df = pd.read_csv(os.path.join(DATA_DIR, train_dataset))
 
 # Construct input and output datasets
+# X_train = train_df.loc[:, [str(i) for i in range(-49, 1)]].to_numpy()
 X_train = train_df.loc[:, [str(i) for i in range(-299, 1)]].to_numpy()
 y_train = train_df.loc[:, [str(i) for i in range(1, 301)]].to_numpy()
+
+X_train2 = train_df.loc[:, [str(i) for i in range(1, 301)]].to_numpy(dtype=np.float32)
+y_train2 = train_df.loc[:, [f"r_{r}_pos_{i}" for r in range (1, 10) for i in range(1, 301)]].to_numpy(dtype=np.float32)
 
 # Check and clean missing values
 X_train = clean_input(X_train)
 y_train = clean_input(y_train)
 
+X_train2 = clean_input(X_train2)
+y_train2 = clean_input(y_train2)
+
 # Data Normalization
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
+
+scaler2 = StandardScaler()
+X_train2 = scaler2.fit_transform(X_train2)
 
 # Loading testing dataset
 test_df = pd.read_csv(os.path.join(DATA_DIR, "test.csv"))
@@ -46,10 +56,11 @@ X_test = scaler.transform(X_test)
 
 # Split dataset for training and prediction
 tX, vX, ty, vy = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+tX2, vX2, ty2, vy2 = train_test_split(X_train2, y_train2, test_size=0.2, random_state=42)
 
 # Training MLP Model
 print("Training MLP Model...")
-model = MLPRegressor(
+model1 = MLPRegressor(
     hidden_layer_sizes=(512, 256),
     activation='tanh',
     solver='adam',
@@ -59,18 +70,36 @@ model = MLPRegressor(
     verbose=True,
     alpha=1e-4
 )
-model.fit(tX, ty)
+model1.fit(tX, ty)
+
+model2 = MLPRegressor(
+    hidden_layer_sizes=(512, 256),
+    activation='tanh',
+    solver='adam',
+    learning_rate_init=0.001,
+    max_iter=2000,
+    random_state=42,
+    verbose=True,
+    alpha=1e-4
+)
+model2.fit(tX2, ty2)
 
 # Check MSE
-val_pred = model.predict(vX)
+val_pred = model1.predict(vX)
 mse = mean_squared_error(vy, val_pred)
 print(f"MSE: {mse:.5f}")
 
 # Prediction
-y_pred = model.predict(X_test)
+y_pred = model1.predict(X_test)
+X_test2 = scaler2.transform(y_pred)
+y_pred_r_1_to_9 = model2.predict(X_test2)
+
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-filename = f"y_pred_mlp_{timestamp}.npy"
-np.save(filename, y_pred)
+filename1 = f"y_pred_mlp_{timestamp}.npy"
+filename2 = f"y_pred_r_1_to_9_mlp_merged_{timestamp}.npy"
+np.save(filename1, y_pred)
+np.save(filename2, y_pred_r_1_to_9)
 
 print(f"Prediction completed and the result has been saved to: y_pred_mlp_{timestamp}.npy, shape={y_pred.shape}")
+print(f"Prediction completed and the result has been saved to: y_pred_r_1_to_9_mlp_merged_{timestamp}.npy, shape={y_pred_r_1_to_9.shape}")
 prepare_submission.prepareSubmission(y_pred, "../../submission_record", "mlp")
